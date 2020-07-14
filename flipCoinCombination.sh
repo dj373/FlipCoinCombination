ebcf906b4927d80d4d0f134cf35634f19c2e4782
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-14 12:07:19 +0530

    Calculated percentage of Singleton Combinations

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,10 +3,20 @@ function flipCoin() {
 	echo $(( RANDOM%2 ))
 }
 isHeads=1
-coinToss="$( flipCoin )"
-if [ $coinToss -eq $isHeads ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A combinations
+for (( ctr=1; ctr<=20; ctr++ ))
+do
+	coinToss="$( flipCoin )"
+	if [ $coinToss -eq $isHeads ]
+	then
+		(( combinations["H"]++ ))
+	else
+		(( combinations["T"]++ ))
+	fi
+done
+echo ${combinations[@]}
+for key in ${!combinations[@]}
+do
+	percent=$(( ${combinations[$key]}*5 ))
+	echo "%of $key is $percent%"
+done
