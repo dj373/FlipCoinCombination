934c7ba3a56c601583baeadb8e96f67bdb633599
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-14 13:03:23 +0530

    Calculated percentage of Triplet Combinations

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -8,26 +8,48 @@ for (( ctr=1; ctr<=20; ctr++ ))
 do
 	coinToss1="$( flipCoin )"
 	coinToss2="$( flipCoin )"
+	coinToss3="$( flipCoin )"
 	if [ $coinToss1 -eq $isHeads ]
 	then
 		if [ $coinToss2 -eq $isHeads ]
 		then
-			(( combinations["HH"]++ ))
+			if [ $coinToss3 -eq $isHeads ]
+			then
+				(( combinations["HHH"]++ ))
+			else
+				(( combinations["HHT"]++ ))
+			fi
 		else
-			(( combinations["HT"]++ ))
+			if [ $coinToss3 -eq $isHeads ]
+			then
+				(( combinations["HTH"]++ ))
+			else
+				(( combinations["HTT"]++ ))
+			fi
 		fi
 	else
 
 		if [ $coinToss2 -eq $isHeads ]
 		then
-			(( combinations["TH"]++ ))
+
+			if [ $coinToss3 -eq $isHeads ]
+			then
+				(( combinations["THH"]++ ))
+			else
+				(( combinations["THT"]++ ))
+			fi
 		else
-			(( combinations["TT"]++ ))
+			if [ $coinToss3 -eq $isHeads ]
+			then
+				(( combinations["TTH"]++ ))
+			else
+				(( combinations["TTT"]++ ))
+			fi
 		fi
 	fi
 done
 echo ${combinations[@]}
-echo "Doublet Combinations"
+echo "Triplet Combinations"
 for key in ${!combinations[@]}
 do
 	percent=$(( ${combinations[$key]}*5 ))
