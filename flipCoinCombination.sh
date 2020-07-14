5ddad935b0dcca94343171d156fda38508c6a8e8
Dhananjay Sachdeva <dhananjay.sachdeva@gmail.com>
2020-07-14 12:54:12 +0530

    Calculated percentage for doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -6,15 +6,28 @@ isHeads=1
 declare -A combinations
 for (( ctr=1; ctr<=20; ctr++ ))
 do
-	coinToss="$( flipCoin )"
-	if [ $coinToss -eq $isHeads ]
+	coinToss1="$( flipCoin )"
+	coinToss2="$( flipCoin )"
+	if [ $coinToss1 -eq $isHeads ]
 	then
-		(( combinations["H"]++ ))
+		if [ $coinToss2 -eq $isHeads ]
+		then
+			(( combinations["HH"]++ ))
+		else
+			(( combinations["HT"]++ ))
+		fi
 	else
-		(( combinations["T"]++ ))
+
+		if [ $coinToss2 -eq $isHeads ]
+		then
+			(( combinations["TH"]++ ))
+		else
+			(( combinations["TT"]++ ))
+		fi
 	fi
 done
 echo ${combinations[@]}
+echo "Doublet Combinations"
 for key in ${!combinations[@]}
 do
 	percent=$(( ${combinations[$key]}*5 ))
